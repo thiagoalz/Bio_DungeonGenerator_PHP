fb0accc1fc9493c2c738459a4e830da44032fb21
Thiago Alvarenga Lechuga <thiagoalz@gmail.com>
2011-07-08 18:53:33 -0300

    Starting the first 'walker'. Not even close yet

--- DungeonGenerator.php
+++ DungeonGenerator.php
@@ -1,4 +1,8 @@
 <?php
+$map=createMap(10,10);
+$map=drunkMan($map,10);
+
+printMap($map);
 
 function createMap($lines,$columns){
 	$myMap=null;
@@ -10,3 +14,44 @@ function createMap($lines,$columns){
 	}
 	return $myMap;
 }
+
+function pickStartPoint($myMap) {
+	$lines=sizeof($myMap);
+	$columns=sizeof($myMap[0]);
+
+	
+	//sortear um ponto de entrada
+	//Tem que ser 0 ou max na linha ou na coluna
+}
+
+function drunkMan($myMap ,$stepNumber){
+	$xPoint=0;
+	$yPoint=0;
+
+	$steps=0;
+
+	while ($steps<$stepNumber){
+		$myMap=visit($xPoint,$yPoint, $myMap);
+
+		$steps++;
+		//ExcolheproximoPonto
+	}
+
+	return $myMap;
+}
+
+function visit($xPoint,$yPoint, $myMap){
+	$myMap[$xPoint][$yPoint]=".";
+
+	return $myMap;
+}
+
+function printMap($map){
+	foreach($map as $line){
+		foreach($line as $item){
+			echo($item);
+		}
+		echo("<BR>");
+	}
+}
+
